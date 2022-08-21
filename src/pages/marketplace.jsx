5fc954bb365a2ea87fd174621da603934dd2dce0
Declan Nnadozie <zedonbiz@gmail.com>
2022-08-21 09:35:36 +0100

    update

--- src/pages/marketplace.jsx
+++ src/pages/marketplace.jsx
@@ -35,7 +35,7 @@ export default function MarketPlace() {
             </a>
             <div className="p-5">
               <a href="#">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                   {v.attribute?.name}
                 </h5>
               </a>
